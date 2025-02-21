4a08cf277ae42da9e5dd8d7865a8b8edd7151157
Daniel Soares <soares.dandansss@gmail.com>
2025-02-21 09:13:16 -0300

    commit

--- Projeto Web API Java Spring Boot/CriandoAPI/projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ Projeto Web API Java Spring Boot/CriandoAPI/projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -1,15 +1,14 @@
 package controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/usuarios")
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
-
     @GetMapping
-    public String texto () {
-        return "Acessando a api";
+    public String listarUsuarios() {
+        return "Lista de usuários";
     }
-    
 }
